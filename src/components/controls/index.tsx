16f8fc21192840e2e2d191648b3b1077bcd3b023
LordFoogThe4rd <89643664+LordFoogThe4rd@users.noreply.github.com>
2026-06-29 09:14:53 +0700

    chore(components): remove unused barrel index files

--- src/components/controls/index.tsx
+++
@@ -1,5 +0,0 @@
-export { InputSlider } from './InputSlider';
-export { InputBox } from './InputBox';
-export { SelectBox, SelectBoxTemplate } from './SelectBox';
-export { Checkbox } from './Checkbox';
-export { CollapsibleGroup } from './CollapsibleGroup';
